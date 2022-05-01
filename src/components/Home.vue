6cb27aac5fa22dd091ffd6ac3b208d3153954cac
vickypineli <v.pinero70@gmail.com>
2022-05-01 19:38:59 +0200

    .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,10 +9,19 @@
                 :label="label"
                 :total-amount="1000000"
                 :amount="amount"
-            />
+            >
+                <template #graphic>
+                    graphic
+                </template>
+                <template #action>
+                    action
+                </template>
+            </Resume>
         </template>
         <template #movements>
-            <Movements/>
+            <Movements
+                :movements="movements"
+            />
         </template>
     </Layout>
 </template>
@@ -21,7 +30,7 @@
 import Layout from "./Layout.vue";
 import Header from "./Header.vue";
 import Resume from "./Resume/Index.vue";
-import Movements from "./Movements.vue";
+import Movements from "./Movements/Index.vue";
 export default {
     components: {
         Layout,
@@ -33,6 +42,47 @@ export default {
         return {
             label: null,
             amount: null,
+            movements: [{
+                id: 0,
+                title: "Movimiento 1",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 1,
+                title: "Movimiento 2",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 2,
+                title: "Movimiento 3",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 3,
+                title: "Movimiento 4",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 4,
+                title: "Movimiento 5",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 6,
+                title: "Movimiento 7",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 7,
+                title: "Movimiento 8",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 8,
+                title: "Movimiento 9",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }]
         }
     },
 };
--- src/components/Movements.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div class="movements">Movimientos</div>
-</template>
